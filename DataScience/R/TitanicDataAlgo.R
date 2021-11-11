55ce195134f7a532c49aeefcd93b290d2d710f40
Kacper Sobczak <57370971+lyffski@users.noreply.github.com>
2021-11-11 19:23:01 +0100

    Update TitanicDataAlgo.R

--- DataScience/R/TitanicDataAlgo.R
+++ DataScience/R/TitanicDataAlgo.R
@@ -1,27 +1,3 @@
-#
-# Copyright 2016 Dave Langer
-#    
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# 
-
-
-
-#
-# This R source code file corresponds to video 7 of the YouTube series
-# "Introduction to Data Science with R" located at the following URL:
-#     https://youtu.be/fuB7s19g3nQ     
-#
-
 
 # Load raw data
 train <- read.csv("train.csv", header = TRUE)
